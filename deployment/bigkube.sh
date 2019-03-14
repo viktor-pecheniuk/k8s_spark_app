892826cdf720e0571c4405c4f9db6ab43aceb244
Viktor Pecheniuk <vpec@ciklum.com>
2019-03-14 14:11:44 +0200

    * changes in deployments

--- deployment/bigkube.sh
+++ deployment/bigkube.sh
@@ -10,29 +10,36 @@ function serve_jar_directory () {
     docker rm nginx-jar-server -f
     local PARENT_DIR=$(cd ../ && pwd)
     docker run --name nginx-jar-server -v ${PARENT_DIR}/target/scala-2.11:/usr/share/nginx/html:ro -d -p 8080:80 nginx
+    echo "don't forget to pass following ip into your Spark CRD declaration to mainApplicationFile field:"
+    ifconfig en0 | awk '$1 == "inet" {print $2}'
 }
 
 function init_spark_operator() {
     kubectl create clusterrolebinding default --clusterrole=edit --serviceaccount=default:default --namespace=default
-    helm init &&
+    helm init --wait &&
     helm install incubator/sparkoperator --namespace spark-operator --set enableWebhook=true --set rbac.create=false
 }
 
 function drop_spark_operator() {
+    kubectl delete -f spark-prometheus.yaml
     helm delete $(helm list --namespace=spark-operator --short)
 }
 
 function create() {
-    kubectl create -f ./mssql.yaml && wait
-    kubectl create -f ./zookeeper.yaml && wait
-    kubectl create -f ./hdfs.yaml && wait
-    kubectl create -f ./kafka.yaml && wait
-    kubectl create -f ./metastore.yaml && wait
-    kubectl create -f ./presto.yaml && wait
+    kubectl create secret generic mssql-user --from-literal=user=sa
+    kubectl create secret generic mssql-password --from-literal=password=YOUR_PASSWORD_123_abcd
+    kubectl create configmap hive-env --from-env-file hive.env --dry-run -o yaml | kubectl apply -f -
+    kubectl create -f mssql.yaml && wait
+    local MSSQL_NODE_PORT=$(kubectl get svc mssql -o=jsonpath='{.spec.ports[?(@.port==1433)].nodePort}')
+    sqlcmd -S $(minikube ip),MSSQL_NODE_PORT -U sa -P YOUR_PASSWORD_123_abcd -i create_db.sql
+    kubectl create -f kafka.yaml && wait
+    kubectl create -f hdfs.yaml && wait
+    kubectl create -f metastore.yaml && wait
+    kubectl create -f presto.yaml && wait
 }
 
 function delete() {
-    kubectl delete -f minipipe
+    kubectl delete -f ./
 }
 
 cd "$(dirname "$0")"
